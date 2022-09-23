2ffe23325f9e20c727bc21ef7e57f147ecd22318
sourasish22 <114250207+sourasish22@users.noreply.github.com>
2022-09-23 22:15:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ import { MdAddModerator } from "react-icons/md";
 import { SiProbot } from "react-icons/si";
 import { TbMessage2Code } from "react-icons/tb";
 const config = {
-  name: "ProBot",
+  name: "CRAFT MUSIC",
   //BotName
   inviteLink:
-    "https://discord.com/oauth2/authorize?client_id=879602399067660338&permissions=8&scope=bot&redirect_uri=https://docs.markos.ga&response_type=code",
+    "https://discord.com/api/oauth2/authorize?client_id=1022367624031645786&permissions=8&scope=bot",
   //BotInvite Link
   Features: [
     {
@@ -38,8 +38,8 @@ const config = {
     // You can added more features if need add
   ],
   //Bot Features
-  serverCount: 300, //-  - Your
-  memberCount: 700, //--   - Bot
+  serverCount: 20, //-  - Your
+  memberCount: 9, //--   - Bot
   executedCommand: 27000, //--  - Stats
   availableCommand: 400, //---   - Here
   //Do not change if you don't know about them
@@ -55,11 +55,8 @@ const config = {
   //----------------------------------------------------------------
   // Your social handle links
   githubLink: "https://github.com/mohitmimani",
-  twitterLink: "https://twitter.com",
-  mediumLink: "https://medium.com/",
-  facebookLink: "https://facebook.com",
-  discordLink: "https://discord.com",
-  youtubeLink: "https://www.youtube.com/channel/UCfhRHeex9lU0ZAVJot6Z2ZQ",
+  discordLink: "https://discord.gg/EfcEmVC2",
+  youtubeLink: "https://www.youtube.com/channel/UCOml3u-76WfEN5p2Q1YZbvg",
 };
 
 export default config;
